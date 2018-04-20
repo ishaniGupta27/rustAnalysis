c31cbd58e47e46e62e86d281462bfb29d20f9f95
Ishani <ishanigupta27@github.com>
2018-04-20 12:36:55 -0700

    Rust Project setup

---
+++ startProj/target/debug/deps/startProj-16e8537d889a5be7.d
@@ -0,0 +1,5 @@
+/Users/ishaniGupta/Library/Mobile Documents/com~apple~CloudDocs/UCSB_3/Runtime System/Code/rustAnalysis/startProj/target/debug/deps/startProj-16e8537d889a5be7: src/main.rs
+
+/Users/ishaniGupta/Library/Mobile Documents/com~apple~CloudDocs/UCSB_3/Runtime System/Code/rustAnalysis/startProj/target/debug/deps/startProj-16e8537d889a5be7.d: src/main.rs
+
+src/main.rs:
---
+++ startProj/target/debug/deps/startProj-57dcc72eb7ffd7ae.d
@@ -0,0 +1,5 @@
+/Users/ishaniGupta/Library/Mobile Documents/com~apple~CloudDocs/UCSB_3/Runtime System/Code/rustAnalysis/startProj/target/debug/deps/libstartProj-57dcc72eb7ffd7ae.rlib: src/lib.rs
+
+/Users/ishaniGupta/Library/Mobile Documents/com~apple~CloudDocs/UCSB_3/Runtime System/Code/rustAnalysis/startProj/target/debug/deps/startProj-57dcc72eb7ffd7ae.d: src/lib.rs
+
+src/lib.rs:
