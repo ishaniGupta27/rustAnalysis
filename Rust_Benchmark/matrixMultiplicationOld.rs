af15e70f08394b8483448af0a580dc9e4ecc91c7
Ishani <ishanigupta27@github.com>
2018-05-11 16:57:47 -0700

    previous benchmarks

--- Rust_Benchmark/matrixMultiplicationOld.rs
+++ Rust_Benchmark/matrixMultiplicationOld.rs
@@ -2,6 +2,10 @@
 // https://gist.githubusercontent.com/csherratt/7634597/raw/b05699f444ab552eb79a7f7c0e1c5ac235c5222c/mat4.rs
 // from
 // https://csherratt.github.io/csherratt/blog/2013/11/24/matrix-multiply-in-rust/
+extern crate rand;
+
+use std::f32;
+use rand::{Rng, thread_rng};
 
 struct Mat1000 {
     dat: [[f32; 1000]; 1000]
@@ -10,7 +14,7 @@ struct Mat1000 {
 impl Mat1000 {
     pub fn mult_m(a: Mat1000, b: &Mat1000) -> Mat1000
     {
-        let mut out = Mat1000 {};
+        let mut out = fill();
 
         for i in 0..1000 {
             for j in 0..1000 {
@@ -23,18 +27,22 @@ impl Mat1000 {
         out
     }
 
-    pub fn fill() -> Mat1000
+    
+}
+
+fn fill() -> Mat1000
     {
-        let mut outRes = Mat1000 {};
+        let mut outRes = Mat1000{dat: [[0.0; 1000]; 1000]} ;
         for i in 0..1000 {
             for j in 0..1000 {
-                outRes.dat[i][j]= 3; //random
+                let num: f32 = thread_rng().gen();
+                //println!("{}",num);
+                outRes.dat[i][j]= num; //random
             }
         }
 
       outRes
     }
-}
 
 fn main()
 {
@@ -43,7 +51,7 @@ fn main()
     let b = fill();
 
     for _ in 0..100_000 {
-        a = Mat4::mult_m(a, &b);
+        a = Mat1000::mult_m(a, &b);
     }
 
 }
