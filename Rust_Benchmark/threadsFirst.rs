14f73e31c5845fe0680a285f6e7a90d33ebe95c2
Ishani <ishanigupta27@github.com>
2018-05-28 16:30:38 -0700

    adding generics

--- Rust_Benchmark/threadsFirst.rs
+++ Rust_Benchmark/threadsFirst.rs
@@ -1,15 +1,18 @@
 use std::thread;
 use std::time::Duration;
-
+use std::env;
 use std::sync::mpsc;
 
 
 
 
 fn main() {
+	let args: Vec<String> = env::args().collect();
 	let (tx, rx): (mpsc::Sender<i32>, mpsc::Receiver<i32>) = mpsc::channel();
-	const n: usize=200; //total slots
-	let t=4;  //threads number
+	let a=&args[1];
+	//let b=&args[2];
+	const n: usize=20000;//a.parse::<usize>().unwrap(); //20000; //total slots
+	let t: usize=a.parse::<usize>().unwrap();//threads number
 	let mut array: [i32; n]= [0; n] ;
 	for i in 0..n { //index starts at 0, 200 not included
 		array[i]=i as i32;
@@ -18,7 +21,7 @@ fn main() {
 	
 
 	let slot_size= n/t;
-	println!("hi numbers {} handled by single thread", slot_size);
+	//println!("hi numbers {} handled by single thread", slot_size);
 	let mut start =0;
 	let mut end=slot_size-1;
 	for j in 0..t{ //8 not included
@@ -29,14 +32,14 @@ fn main() {
 		    let inside_end=end;
 		    let mut sum=0;
 		    
-		    println!("hi going to sum from {} to {} from the spawned thread!", inside_start,inside_end);
+		    //println!("hi going to sum from {} to {} from the spawned thread!", inside_start,inside_end);
 			for c in inside_start..(inside_end+1) {
 	            sum=sum+array[c];
 	            //println!("Going to sleep");
 	            //thread::sleep_ms(100);
 	            //println!("Got up");
 	        }
-	        println!("hi going to sum from {} to {} from the spawned thread! total..{}", inside_start,inside_end,sum);
+	        //println!("hi going to sum from {} to {} from the spawned thread! total..{}", inside_start,inside_end,sum);
 	        child_tx.send(sum);
 
 		});
