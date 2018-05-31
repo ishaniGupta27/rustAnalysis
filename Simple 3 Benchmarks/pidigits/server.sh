a89e0c81c513329dcec56e117bb5c95f892609a9
Ishani <ishanigupta27@github.com>
2018-05-31 13:20:47 -0700

    different input for pidigits

--- Simple 3 Benchmarks/pidigits/server.sh
+++ Simple 3 Benchmarks/pidigits/server.sh
@@ -2,46 +2,48 @@
 
 repeats=50
 output_file='resultsTime.txt'
-input=2500
-while read exec; do
-    
-	command_to_run="./Executables/"$exec" "$input"";
-	echo 'Benchmarking ' $command_to_run;
-    sum=0.0
-	for (( i = 1; i <= $repeats ; i++ ))
-    do
-        # percentage completion
-        p=$(( $i * 100 / $repeats))
-        # indicator of progress
-        l=$(seq -s "+" $i | sed 's/[0-9]//g')
 
-        # runs time function for the called script, output in a comma seperated
-        # format output file specified with -o command and -a specifies append
-        #/usr/bin/time -lp "%E,%U,%S" -o ${output_file} -a ${command_to_run} > ${output_file} 2>&1
-        #curl https://sh.rustup.rs -sSf | sh
-       	#(time ${command_to_run}) 2>>${output_file} #>> will append whereas > will wipe out !
-    	#utime="$( TIMEFORMAT='%U';time ${command_to_run} 2>&1 1>/dev/null )"
-        #echo 'usertime' $utime
+while read input; do
+    while read exec; do
+        
+    	command_to_run="./Executables/"$exec" "$input"";
+    	echo 'Benchmarking ' $command_to_run;
+        sum=0.0
+    	for (( i = 1; i <= $repeats ; i++ ))
+        do
+            # percentage completion
+            p=$(( $i * 100 / $repeats))
+            # indicator of progress
+            l=$(seq -s "+" $i | sed 's/[0-9]//g')
 
-        #stime="$( TIMEFORMAT='%S';time ${command_to_run} 2>&1 1>/dev/null )"
-        #echo 'systime' $stime
+            # runs time function for the called script, output in a comma seperated
+            # format output file specified with -o command and -a specifies append
+            #/usr/bin/time -lp "%E,%U,%S" -o ${output_file} -a ${command_to_run} > ${output_file} 2>&1
+            #curl https://sh.rustup.rs -sSf | sh
+           	#(time ${command_to_run}) 2>>${output_file} #>> will append whereas > will wipe out !
+        	#utime="$( TIMEFORMAT='%U';time ${command_to_run} 2>&1 1>/dev/null )"
+            #echo 'usertime' $utime
 
-        #echo 'ttime'
-        ttime=$(echo $( TIMEFORMAT="%3U + %3S"; { time ${command_to_run}; } 2>&1 1>/dev/null) "*1000" | bc -l)
-        #echo "$ttime"
+            #stime="$( TIMEFORMAT='%S';time ${command_to_run} 2>&1 1>/dev/null )"
+            #echo 'systime' $stime
 
-        sum=`echo "$sum+$ttime" | bc -l`
+            #echo 'ttime'
+            ttime=$(echo $( TIMEFORMAT="%3U + %3S"; { time ${command_to_run}; } 2>&1 1>/dev/null) "*1000" | bc -l)
+            #echo "$ttime"
 
-        #echo -ne ${l}' ('${p}'%) \r' #for the progress
-    done;
-    #echo 'sum'
-    echo 'Test For..' $exec >> $output_file
-    echo 'Total Time..' $sum >> $output_file
-    echo 'Total Iterations..' $repeats >> $output_file
-    echo 'Input..' $input >> $output_file
-    #echo -ne '\n'
+            sum=`echo "$sum+$ttime" | bc -l`
 
-    # Convenience seperator for file
-    echo '--------------------------' >> $output_file
+            #echo -ne ${l}' ('${p}'%) \r' #for the progress
+        done;
+        #echo 'sum'
+        echo 'Test For..' $exec >> $output_file
+        echo 'Total Time..' $sum >> $output_file
+        echo 'Total Iterations..' $repeats >> $output_file
+        echo 'Input..' $input >> $output_file
+        #echo -ne '\n'
 
-done < execToRun.txt
+        # Convenience seperator for file
+        echo '--------------------------' >> $output_file
+
+    done < execToRun.txt
+done < inputFile.txt
